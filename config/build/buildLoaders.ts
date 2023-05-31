023216900cfa64c629ef1d2303ecfa8dc90bc145
mun-a <karnevall.13.13@gmail.com>
2023-05-31 18:38:34 +0500

    add babel and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,32 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
         use: ["@svgr/webpack"],
+    };
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ["@babel/preset-env"],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ["ru", "en"],
+                            keyAsDefaultValue: true,
+                            discardOldKeys: true,
+                        },
+                    ],
+                ],
+            },
+        },
     };
 
     const cssLoader = {
@@ -30,11 +51,11 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
-    // Если не используем ts - нужен babel-loader
+    // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: /node-modules/,
+        exclude: /node_modules/,
     };
 
     const fileLoader = {
@@ -46,5 +67,6 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
-    return [typescriptLoader, cssLoader, svgLoader, fileLoader];
+    // Важен порядок: если перед babelLoader подключить tsLoader, то будет ошибка
+    return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
