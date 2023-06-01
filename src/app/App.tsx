3272376cee21bee3e9767e9d337f3dcf9a4c1df7
mun-a <karnevall.13.13@gmail.com>
2023-06-01 11:46:18 +0500

    add eslint and fix all files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,18 @@
-import "./styles/index.scss";
-import { useTheme } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
-import { classNames } from "shared/lib/classNames/classNames";
-import { AppRouter } from "app/providers/router";
-import { Navbar } from "widgets/Navbar";
-import { Sidebar } from "widgets/Sidebar";
-import { Suspense } from "react";
+import './styles/index.scss';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 const App = () => {
     const { theme } = useTheme();
 
     return (
-        <div className={classNames("app", {}, [theme])}>
+        <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,4 +1,4 @@
-declare module "*.scss" {
+declare module '*.scss' {
     interface IClassNames {
         [className: string]: string;
     }
@@ -6,14 +6,16 @@ declare module "*.scss" {
     export = classNames;
 }
 
-declare module "*.svg" {
+declare module '*.svg' {
+    import React from 'react';
+
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
     export default content;
 }
 
-declare module "*.png";
-declare module "*.gif";
-declare module "*.jpeg";
-declare module "*.jpg";
+declare module '*.png';
+declare module '*.gif';
+declare module '*.jpeg';
+declare module '*.jpg';
 
 declare const __IS_DEV__: boolean;
