590d27e6dc2125843ab011c8716c7be291f1a6ea
mun-a <karnevall.13.13@gmail.com>
2023-06-02 18:06:47 +0500

    fix linter and story

--- src/app/providers/ErrorBoundary/ui/BugButton.tsx
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -1,9 +1,11 @@
 import { Button } from 'shared/ui/Button/Button';
 import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 // Компонент для тестирования
 export const BugButton = () => {
     const [error, setError] = useState(false);
+    const { t } = useTranslation();
 
     const onThrow = () => setError(true);
 
@@ -13,5 +15,5 @@ export const BugButton = () => {
         }
     }, [error]);
 
-    return <Button onClick={onThrow}>throw error</Button>;
+    return <Button onClick={onThrow}>{t('throw error')}</Button>;
 };
--- src/shared/ui/AppLink/AppLink.stories.tsx
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -8,9 +8,6 @@ const meta: Meta<typeof AppLink> = {
     title: 'shared/AppLink',
     component: AppLink,
     tags: ['autodocs'],
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
     args: {
         to: '/',
     },
