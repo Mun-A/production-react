c351f5edcc6e8a2de76d7e48cb0db1dc8e2fa8a6
mun-a <karnevall.13.13@gmail.com>
2023-06-06 11:17:56 +0500

    fix imports

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,5 +1,5 @@
-import { counterReducer } from './model/slice/CounterSlice';
 import { Counter } from './ui/Counter';
 import type { CounterSchema } from './model/types/CounterSchema';
+import { counterReducer } from './model/slice/counterSlice';
 
 export { counterReducer, Counter, CounterSchema };
