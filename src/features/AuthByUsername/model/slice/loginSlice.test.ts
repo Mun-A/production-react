d70a5d12d02e10246718d68a891f5e4046d3e702
mun-a <karnevall.13.13@gmail.com>
2023-06-08 11:07:39 +0500

    add tests for authByUsername, test async thunk

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,25 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+import { loginByUsername } from '../services/loginByUsername/loginByUsername';
+
+describe('loginSlice', () => {
+    test('Test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: '123' };
+
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('123123'),
+        )).toEqual({ username: '123123' });
+    });
+    test('Test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+
+        expect(
+            loginReducer(
+                state as LoginSchema,
+                loginActions.setPassword('123123'),
+            ),
+        ).toEqual({ password: '123123' });
+    });
+});
