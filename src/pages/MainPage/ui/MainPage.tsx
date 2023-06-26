d553d42215b322a89f2c44cbc7953496285eb3d5
mun-a <karnevall.13.13@gmail.com>
2023-06-26 08:09:33 +0500

    add headless ui and listbox component

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,15 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Page } from 'widgets/Page/Page';
 
 const MainPage = memo(() => {
     const { t } = useTranslation('main');
 
-    return <div>{t('Главная страница')}</div>;
+    return (
+        <Page>
+            {t('Главная страница')}
+        </Page>
+    );
 });
 
 export default MainPage;
