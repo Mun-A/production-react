8e1aeb1333b7b2b51e72ad8ada4bed83d9b4b7be
mun-a <karnevall.13.13@gmail.com>
2023-07-01 15:59:22 +0500

    add profile card tests, user event and conditional virtualization

--- src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
@@ -126,6 +126,7 @@ export const EditableProfileCard = memo((props: EditableProfileCardProps) => {
                             key={err}
                             theme={TextTheme.ERROR}
                             text={validateErrorTranslations[err]}
+                            data-testid="EditableProfileCard.Error"
                         />
                     ))}
                 <ProfileCard
