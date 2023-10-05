c865a0f57d4d4447acee76a2326f981991fb7c3e
mun-a <karnevall.13.13@gmail.com>
2023-10-05 14:43:06 +0500

    add json settings

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,22 +6,24 @@ import { Navbar } from '@/widgets/Navbar';
 import { Sidebar } from '@/widgets/Sidebar';
 import { getUserInited, userActions } from '@/entities/User';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 
 const App = () => {
+    const { theme } = useTheme();
     const dispatch = useAppDispatch();
-    const initied = useSelector(getUserInited);
+    const inited = useSelector(getUserInited);
 
     useEffect(() => {
         dispatch(userActions.initAuthData());
     }, [dispatch]);
 
     return (
-        <div className={classNames('app', {}, [])}>
+        <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
-                    {initied && <AppRouter />}
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
